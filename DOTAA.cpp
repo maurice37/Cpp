5a568e034d3d00124dafc3750a6ad469f29c6418
maurice37 <mauricepatel37@gmail.com>
2014-12-11 00:20:45 +0530

    Modified DOTAA solution and optimized space complexity

--- DOTAA.cpp
+++ DOTAA.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-int fill(int t,int d){
+int fill(const int& t,const int& d){
 	int a=0;
 	if(t==d)return a;
 	else{
@@ -14,22 +14,24 @@ int fill(int t,int d){
 		return a;
 	}
 }
+/*
 void ans(int sum,int m){
 	if(sum>=m)printf("YES\n");
 	else printf("NO\n");
-}
+}*/
 int main(int argc, char const *argv[]){
 	int t;
 	scanf("%d",&t);
 	while(t--){
 		int n,m,d,temp,sum=0;
 		scanf("%d %d %d",&n,&m,&d);
-		int *h=new int[n];
+		//int *h=new int[n];
 		for(int i=0;i<n;i++){
 			scanf("%d",&temp);
 			sum+=fill(temp,d);
 		}
-		ans(sum,m);
+		if(sum>=m)printf("YES\n");
+		else printf("NO\n");
 	}
 
 	return 0;
